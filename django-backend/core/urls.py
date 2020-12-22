717d648a10f933ecf1e22869902ea49f31c00349
oleg <gusevoleg96@gmail.ru>
2020-12-22 12:49:09 +0300

    first

---
+++ django-backend/core/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+  path('api/ping/', views.GetPongView.as_view()),
+  path('', views.index, name='index'),
+
+]
---
+++ django-backend/core/views.py
@@ -0,0 +1,45 @@
+import redis
+from django.http import HttpResponse
+from django.shortcuts import render
+from django.utils.decorators import classonlymethod
+from django.views import View
+
+import asyncio
+from datetime import timedelta
+from redis import Redis
+
+redis = redis.Redis(host='localhost', port=6379, db=0)
+key = "PING"
+limit = 10
+period = timedelta(seconds=10)
+
+
+def request_is_limited(r: Redis, k: str, lim: int, p: timedelta):
+    if r.setnx(key, limit):
+        r.expire(key, int(period.total_seconds()))
+    bucket_val = r.get(key)
+    if bucket_val and int(bucket_val) > 0:
+        r.decrby(key, 1)
+        return False
+    return True
+
+
+class GetPongView(View):
+    @classonlymethod
+    def as_view(cls, **initkwargs):
+        view = super().as_view(**initkwargs)
+        view._is_coroutine = asyncio.coroutines._is_coroutine
+        return view
+
+    async def get(self, request, *args, **kwargs):
+        if request_is_limited(redis, key, limit, period):
+            return HttpResponse("Too many requests, please try again later.", status=429)
+        return HttpResponse("PONG", status=200)
+
+def index(request):
+    context = {}
+    return render(request, 'index.html', context)
+
+
+
+
