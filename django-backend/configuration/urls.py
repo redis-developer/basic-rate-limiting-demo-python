0aa8fa879fab19f401f2bf15a7303b30613edcc0
oleg <olegsh-gusev@mail.ru>
2020-12-23 15:18:56 +0300

    add favicon

--- django-backend/configuration/urls.py
+++ django-backend/configuration/urls.py
@@ -24,5 +24,5 @@ urlpatterns = [
     path('', include('core.urls')),
 
 
-]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
