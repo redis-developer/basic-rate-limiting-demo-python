ebc7b7042f2826360c8eb93f83ad29adb02e3042
oleg <gusevoleg96@gmail.ru>
2021-01-13 17:49:50 +0300

    without DATABASES

--- configuration/settings.py
+++ configuration/settings.py
@@ -84,12 +84,7 @@ WSGI_APPLICATION = 'configuration.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+DATABASES = {}
 
 
 # Password validation
