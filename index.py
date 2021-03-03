2c7b7c03cc888354a6eff41bb74522d395d76b59
Ruslan <rvagapov@deliveryweb.ru>
2021-03-03 20:04:53 +0300

    try fix vercel deploy

--- index.py
+++ index.py
@@ -2,6 +2,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'configuration.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'server.configuration.settings')
 
 app = get_wsgi_application()
